2f9424d8521950ed3054292991540a8966360c94
UriceMergulhao <80512744+UriceMergulhao@users.noreply.github.com>
2021-08-28 18:36:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas("myCanvas")
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+
+    <center>
+		<h2 class="btn-primary">MINECRAFT</h2>
+
+		<div id="head_info" class="btn-danger">
+			<h3>Current Width  = <span id="current_width">30</span></h3>
+			<h3>Current Height  = <span id="current_height">30</span></h3>
+		</div>
+
+		<canvas width="1000" height="600" id="myCanvas"></canvas>
+
+	</center>
+	
+}
+
+function playSound(){
+	
+}
